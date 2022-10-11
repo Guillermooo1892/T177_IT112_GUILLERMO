75dd8aa3d207ad1e9166b5e1a3ef14f6044e122f
Guillermooo1892 <115524742+Guillermooo1892@users.noreply.github.com>
2022-10-11 17:32:24 +0800

    Add files via upload

---
+++ Hello_World.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main () {
+
+
+
+    int money = 1500 ;
+    float temp = 36.6 ;
+    char letter = 'L' ;
+
+
+    printf("the total money is %d and the temp is %.2f\n", money, temp,);
+    printf("value of letter is %c" , letter) ;
+
+    return 0;
+}
+
